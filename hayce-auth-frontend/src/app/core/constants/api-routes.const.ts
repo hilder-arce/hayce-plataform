5dee4596111f9972507c061b395d936260f5a60e
hilder-arce <hilderarce566@gmail.com>
2026-03-30 17:19:05 -0500

    change the buton print on mobile

--- hayce-auth-frontend/src/app/core/constants/api-routes.const.ts
+++ hayce-auth-frontend/src/app/core/constants/api-routes.const.ts
@@ -1,8 +1,8 @@
-
-
-export const SOCKET_URL = 'http://localhost:3000';
-export const GRAPHQL_API_URL = 'http://localhost:3000/graphql';
-/*
 export const GRAPHQL_API_URL = 'https://hayce-backend.onrender.com/graphql';
 export const SOCKET_URL = 'https://hayce-backend.onrender.com';
+
+
+/*
+export const SOCKET_URL = 'http://localhost:3000';
+export const GRAPHQL_API_URL = 'http://localhost:3000/graphql';
 */
